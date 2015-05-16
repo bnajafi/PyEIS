647e889d77e4aa433de6c5c16c90825f6f7cb428
Milan Skocic <milan.skocic@gmail.com>
2015-05-16 12:32:03 +0200

    Add version naming.

--- pyeis/version.py
+++ pyeis/version.py
@@ -1,5 +1,14 @@
 MAJOR = 0
 MINOR = 4
-MICRO = 'x'
+MICRO = 'dev'
 
-version = '{:d}.{:d}.{:d}'.format(MAJOR, MINOR, MICRO)
+dev_labels = ['dev', 'alpha', 'beta']
+
+if isinstance(MICRO, str):
+    if MICRO in dev_labels:
+        version = '{:d}.{:d}{:s}'.format(MAJOR, MINOR, MICRO)
+    elif MICRO not in dev_labels':
+        version = '{:d}.{:d}.{:s}'.format(MAJOR, MINOR, MICRO)
+
+elif isinstance(MICRO, int):
+    version = '{:d}.{:d}.{:d}'.format(MAJOR, MINOR, MICRO)
